f5762de777bc1324a47b8bec7acdbf099160bace
c2huc2hu <c2huc2hu@gmail.com>
2016-07-05 15:14:31 -0700

    Support for python 2

--- complex orbits.py
+++ complex orbits.py
@@ -5,7 +5,7 @@
 #
 # Todo:
 # - Implement intercepts (i.e. so the spacecraft actually hits the planet)
-# - 
+# -
 
 import pygame
 import math
@@ -15,11 +15,11 @@ dt = .01  # Time gap for calculating impulse and stuff e.g. (dp = F * dt)
 G = .01  # Gravitational constant
 
 
-class MassiveObject():
+class MassiveObject(object):
     """
     Represents any object that exists in space.
-    Includes: planets, stars, asteroids, ships and possibly missiles. 
-    """    
+    Includes: planets, stars, asteroids, ships and possibly missiles.
+    """
     def __init__(self, x, y, mass, radius, color=0x000000):
         self.x, self.y = x, y
         self.mass = mass
@@ -33,7 +33,7 @@ class MassiveObject():
         Parameters:
         surface - an instance of pygame.surface.Surface that this will be drawn on
         reference_frame - something with x and y. This will be drawn with that in the middle.
-        zoom - the system will be drawn with everything multiplied by zoom. 
+        zoom - the system will be drawn with everything multiplied by zoom.
         """
         if reference_frame is None:
             x = surface.get_width() / 2 + self.x * zoom
@@ -41,7 +41,7 @@ class MassiveObject():
         else:
             x = surface.get_width() / 2 + (self.x - reference_frame.x) * zoom
             y = surface.get_height() / 2 + (self.y - reference_frame.y) * zoom
-        
+
         pygame.draw.circle(surface, self.color, (int(x), int(y)), int(self.radius * zoom ** 0.5))
 
 class FixedObject(MassiveObject):
@@ -56,7 +56,7 @@ class OrbitingObject(MassiveObject):
     Base class for objects that are affected by gravity
     """
     def __init__(self, *args):
-        super().__init__(*args)
+        super(OrbitingObject, self).__init__(*args)
         self.vel_x, self.vel_y = 0, 0
 
     def get_acceleration(self, massive_objects):
@@ -66,10 +66,10 @@ class OrbitingObject(MassiveObject):
         Parameters
         massive_objects - iterable of MassiveObject and FixedObject. If self is in MassiveObject, ignores it.
 
-        return - acceleration as a x,y pair. 
+        return - acceleration as a x,y pair.
         """
         accel_x, accel_y = 0, 0
-        
+
         for obj in massive_objects:
             dist_squared = (self.x - obj.x) ** 2 + (self.y - obj.y) ** 2
             if dist_squared < 0.0001:
@@ -78,7 +78,7 @@ class OrbitingObject(MassiveObject):
 
             accel_x += G * obj.mass / dist_squared * (obj.x - self.x) / math.sqrt(dist_squared)
             accel_y += G * obj.mass / dist_squared * (obj.y - self.y) / math.sqrt(dist_squared)
-            
+
         return accel_x, accel_y
 
     def update(self, acceleration):
@@ -86,7 +86,7 @@ class OrbitingObject(MassiveObject):
         Updates the position and speed of the object
 
         Parameters
-        acceleration - a 2-tuple of the x and y components of acceleration. 
+        acceleration - a 2-tuple of the x and y components of acceleration.
         """
         self.vel_x += acceleration[0] * dt
         self.vel_y += acceleration[1] * dt
@@ -129,7 +129,7 @@ class OrbitingObject(MassiveObject):
 
 class Spacecraft(OrbitingObject):
     import math
-    
+
     class Burn():
         def __init__(self, delta_vx, delta_vy, time):
             """
@@ -148,17 +148,17 @@ class Spacecraft(OrbitingObject):
     # STATES
     TRANSFERRING = "<spacecraft state:transferring>"  # when a Spacecraft is moving between orbits.
     STABLE = "<spacecraft state:transferring>"  #when a Spacecraft is in a stable circular orbit
-        
-    
+
+
     def __init__(self, source, radius, color, mass=0.00001):
         """
         Create a spacecraft at source. It inherits source's velocity, so it
-        should probably orbit around the same thing as source does. 
+        should probably orbit around the same thing as source does.
 
         Parameters:
         source - where the planet will start. must have properties x and y.
         """
-        super().__init__(source.x, source.y, mass, radius, color)
+        super(Spacecraft, self).__init__(source.x, source.y, mass, radius, color)
         try:
             self.vel_x = source.vel_x
             self.vel_y = source.vel_y
@@ -166,28 +166,28 @@ class Spacecraft(OrbitingObject):
             pass  # orbiting around a fixed object.
         self.radius = radius
         self.color = color
-        
+
         self.scheduled_burns = []
 
     def launch_to_orbit(self, centre, target):
         """
         Launch an orbiter to orbit in the same orbit as the target_planet
         Only works on circular orbits because of how the radius is calculated.
-        Clears all other scheduled burns. 
-        
+        Clears all other scheduled burns.
+
         TODO: Make it time correctly to actually hit target planet
 
         Parameters:
-        centre - what the Spacecraft and the target are orbiting. 
+        centre - what the Spacecraft and the target are orbiting.
         target - the orbiter will be launched at a target planet. Must have x, y, x_vel, y_vel
         """
         self.set_circular_orbit(centre, self.get_orbit_direction(centre))
-        
+
         dx_s = self.x - centre.x  # x-distance to the source
         dy_s = self.y - centre.y  # y-distance
         dx_t = target.x - centre.x  # x-distance to the target
         dy_t = target.y - centre.y
-        
+
         r1 = math.sqrt(dx_s * dx_s + dy_s * dy_s)
         r2 = math.sqrt(dx_t * dx_t + dy_t * dy_t)
 
@@ -205,13 +205,13 @@ class Spacecraft(OrbitingObject):
     def launch_to_orbital_target(self, centre, target):
         """
         Launch itself to actually hit target.
-        TODO: implement this. 
+        TODO: implement this.
         """
-        pass 
+        pass
 
     def update(self, acceleration):
-        super().update(acceleration)
-        
+        super(Spacecraft, self).update(acceleration)
+
         #which order do these parts go in?
         for burn in self.scheduled_burns:
             burn.time -= 1
@@ -219,16 +219,16 @@ class Spacecraft(OrbitingObject):
                 self.vel_x += burn.delta_vx
                 self.vel_y += burn.delta_vy
 
-        #Remove all burns with time < 0.         
+        #Remove all burns with time < 0.
         self.scheduled_burns = list(filter(lambda burn:burn.time > 0, self.scheduled_burns))
-        
 
-class Particle():
+
+class Particle(object):
     """
     Base class for objects that do not have gravity of their own.
     """
     pass
-    
+
 pygame.init()
 window = pygame.display.set_mode ((600, 400))
 main_surface = pygame.Surface ((600, 400))
@@ -258,7 +258,7 @@ def render(reference_frame, zoom):
 spaceship_one.launch_to_orbit(sun, planets[2])
 
 while (True):
-    for i in range(100):    
+    for i in range(100):
         for p in planets:
             a = p.get_acceleration([sun])
             p.update(a)
@@ -268,9 +268,9 @@ while (True):
 
         a = spaceship_one.get_acceleration([sun])
         spaceship_one.update(a)
-        
+
     render(sun, .5)
-    
+
     window.blit(main_surface, (0, 0))
     pygame.display.flip()
 
@@ -281,6 +281,6 @@ while (True):
             spaceship_one.launch_to_orbit(sun, planets[1])
         elif event.key == pygame.K_2:
             spaceship_one.launch_to_orbit(sun, planets[2])
-        elif event.key == pygame.K_3:   
+        elif event.key == pygame.K_3:
             spaceship_one.launch_to_orbit(sun, planets[3])
     pygame.event.pump()
